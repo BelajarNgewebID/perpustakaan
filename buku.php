7923835c3650d218e17a6d9de00870970029dfd2
Belajar Ngeweb ID <riyan.satria.619@gmail.com>
2017-04-03 22:29:03 +0700

    Update buku.php

--- buku.php
+++ buku.php
@@ -5,3 +5,18 @@ $sql = mysqli_query($konek, "SELECT * FROM buku WHERE idbuku = '$id'");
 $row = mysqli_fetch_array($sql);
 echo $row['judul'];
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale = 1">
+    <title><?php echo $row['judul']." | ".$konfig['nama_perpus']; ?></title>
+    <script type=" text/javascript" src="../js/jquery-3.1.1.js"></script>
+    <style type="text/css">
+      body { background-color:#ecf0f1; }
+    </style>
+  </head>
+<body>
+  
+  </body>
+</html>
