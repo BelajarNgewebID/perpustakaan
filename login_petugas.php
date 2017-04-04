9f9743dd70418f9a76d56d6bec7bb29bfd8b2092
Belajar Ngeweb ID <riyan.satria.619@gmail.com>
2017-04-04 07:34:40 +0100

    Update login_petugas.php

--- login_petugas.php
+++ login_petugas.php
@@ -1,1 +1,18 @@
 
+<?php
+include 'konfig.php';
+error_reporting(1);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale = 1">
+    <title>Login Petugas | <?php echo $konfig['nama_perpus']; ?></title>
+    <link href="css/style.petugas.css" rel="stylesheet">
+    <script type="text/javascript" src="js/jquery-3.1.1.js"></script>
+    <!--  JavaScript Disini -->
+  </head>
+  <body>
+    
+    <div class="atass
